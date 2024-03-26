57ab035edd67d797569c07d860584b48ef8f3498
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-26 19:14:23 +0200

    refactoring util class

--- src/main/java/ParkingUtil.java
+++ src/main/java/ParkingUtil.java
@@ -1,29 +1,45 @@
 package main.java;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import net.datafaker.Faker;
+import net.datafaker.providers.base.Name;
 
 public class ParkingUtil {
     public final static List<String> numberList = new ArrayList<>();
     public final static Set<String> numberSet = new HashSet<>();
+    public final static Map<String, String> numberMap = new HashMap<>();
     public final static Scanner scan = new Scanner(System.in);
 
+    private final static Faker faker = new Faker(Locale.getDefault());
+
     static {
-        numberList.add("JF8854LK");
-        numberList.add("DD3454OK");
-        numberList.add("SD5667GG");
-        numberList.add("HJ5467FD");
-        numberList.add("VB5567JJ");
+        numberList.add(faker.examplify("AB1111CD"));
+        numberList.add(faker.examplify("AB1111CD"));
+        numberList.add(faker.examplify("AB1111CD"));
+        numberList.add(faker.examplify("AB1111CD"));
+        numberList.add(faker.examplify("AB1111CD"));
     }
 
     static {
-        numberSet.add("JF8854LK");
-        numberSet.add("DD3454OK");
-        numberSet.add("SD5667GG");
-        numberSet.add("HJ5467FD");
-        numberSet.add("VB5567JJ");
+        numberSet.add(faker.examplify("AB1111CD"));
+        numberSet.add(faker.examplify("AB1111CD"));
+        numberSet.add(faker.examplify("AB1111CD"));
+        numberSet.add(faker.examplify("AB1111CD"));
+        numberSet.add(faker.examplify("AB1111CD"));
+    }
+
+    static {
+        numberMap.put(faker.examplify("AB1111CD"), faker.name().fullName());
+        numberMap.put(faker.examplify("AB1111CD"), faker.name().fullName());
+        numberMap.put(faker.examplify("AB1111CD"), faker.name().fullName());
+        numberMap.put(faker.examplify("AB1111CD"), faker.name().fullName());
+        numberMap.put(faker.examplify("AB1111CD"), faker.name().fullName());
     }
 }
