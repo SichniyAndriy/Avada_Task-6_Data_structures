2078168a55d8e0061c2fa94dfd9acbefe41cfe42
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-26 19:15:39 +0200

    add code in parking_4

---
+++ src/main/java/parking_4/Main.java
@@ -0,0 +1,37 @@
+package main.java.parking_4;
+
+import main.java.ParkingUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                        Вітаю в програмі ПАРКІНГ 0.3
+                        Введіть:
+                        стоп - для виходу з програми
+                        список - для виводу всіх номерів та власників
+                        авто - для виводу всіх номерів
+                        власники - для виводу всіх власників
+                        сам номер - для збереження в базі""");
+
+        while(true) {
+            System.out.print("Введіть команду: ");
+            resolveMap(ParkingUtil.scan.nextLine().trim().toUpperCase());
+        }
+    }
+
+    public static void resolveMap(String s) {
+        if (s.matches("^[A-Z]{2}\\d{4}[A-Z]{2}$")) {
+            System.out.print("Введіть ім'я власника: ");
+            String name = ParkingUtil.scan.nextLine();
+            ParkingUtil.numberMap.put(s,name);
+        } else {
+            switch (s) {
+                case "СПИСОК", "LIST" -> ParkingUtil.numberMap.forEach((k, v) -> System.out.println(k + " - " + v) );
+                case "АВТО", "AUTOS" -> ParkingUtil.numberMap.keySet().forEach(System.out::println);
+                case "ВЛАСНИКИ", "OWNERS" -> ParkingUtil.numberMap.values().forEach(System.out::println);
+                case "СТОП", "STOP" -> { ParkingUtil.scan.close(); System.exit(0); }
+                default -> System.out.println("Невідома команда. Перевірте ввод");
+            }
+        }
+    }
+}
