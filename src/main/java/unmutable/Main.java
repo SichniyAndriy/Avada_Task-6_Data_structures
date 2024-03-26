49ec1733918b7d22c910f2e28fff2e2aeb48519c
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-26 14:15:48 +0200

    add unmutable package

---
+++ src/main/java/unmutable/Main.java
@@ -0,0 +1,16 @@
+package unmutable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List unmodifiableList = Collections.unmodifiableList(Collections.EMPTY_LIST);
+        Set unmodifiabledSet = Collections.unmodifiableSet(Collections.EMPTY_SET);
+
+        List copyOfList = List.copyOf(unmodifiableList);
+        Set copyOfSet = Set.copyOf(unmodifiabledSet);
+    }
+}
