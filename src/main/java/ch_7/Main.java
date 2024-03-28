2030ab79b6345982681eec756900a7c8c87321a2
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-28 17:30:44 +0200

    add ch_7 package

---
+++ src/main/java/ch_7/Main.java
@@ -0,0 +1,37 @@
+package main.java.ch_7;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    <T> void meth(T t) {
+
+    }
+
+    <W extends CharSequence&Comparable<String>> void  meth2(W word) {
+        System.out.println(word);
+    }
+}
+
+class User <T1 extends Comparable<T1>, T2 extends Number, V> {
+    T1 x;
+    T2 y;
+    V v;
+
+    public User(T1 x, T2 y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void meth3(List<? extends T1> prod, List<? super T2> cons) {
+        cons.add((T2) prod.getFirst());
+    }
+}
+
+
+class A<U extends User<T1, T2, V>, T2 extends Number, V, T1 extends Comparable<T1>> {
+    U u;
+}
